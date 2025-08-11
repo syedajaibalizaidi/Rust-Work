56c10264e89d7b6363069a66d9f10e9a7e923e54
Syed Ajaib Ali <aleeyzaidi1@gmail.com>
2025-08-11 09:32:20 +0500

    Create bank.rs
    bank.rs code example

---
+++ src/bank.rs
@@ -0,0 +1,53 @@
+#[derive(Debug)] 
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }  
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+}
+
+fn main() {
+    let mut bank = Bank::new();
+    let mut account = Account::new(1, String::from("Bob"));
+
+    account.deposit(500);
+    account.withdraw(230);
+    
+    bank.add_account(account);
+
+    println!("{:#?}", bank);
+}
